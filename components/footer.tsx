eb712ca41dc5949f54b8c6dd707c5781f20d6322
kaltsone <kvjhau@utu.fi>
2022-02-21 15:09:20 +0200

    continuing with the footer, making things more pretty

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/Link';
 import { FC } from 'react';
-import { AiOutlineInstagram } from 'react-icons/ai';
+import { AiOutlineInstagram, AiFillFacebook } from 'react-icons/ai';
+import { FaDiscord, FaTelegramPlane } from 'react-icons/fa';
 
 import { NavigationItem } from 'contentful/client';
 
@@ -12,7 +13,7 @@ interface FooterProps {
 const Footer: FC<FooterProps> = ({ navigationItems }) => {
   return (
     <div className="z-15 p-6 flex flex-wrap items-center justify-center bg-blue-dark">
-      <div className="p-4 px-12 text-l text-white">
+      <div className="p-4 px-16 text-l text-white">
         <ul className="space-y-2 ">
           <li key="/">
             <Link href={'/'}>
@@ -29,7 +30,7 @@ const Footer: FC<FooterProps> = ({ navigationItems }) => {
           {/* Other buttons, placeholders */}
           <li>
             <Link href={'/lisatietoja'}>
-              <a className='hover:underline'>Lisätietoja</a>
+              <a>Lisätietoja</a>
             </Link>
           </li>
           <li>
@@ -44,21 +45,51 @@ const Footer: FC<FooterProps> = ({ navigationItems }) => {
           </li>
         </ul>
       </div>
-      <div className="p-4 px-12 text-sm text-white">
+      <div className="p-4 px-16 text-sm text-white">
         <ul>
-          <li className="text-center">
-            <a href="https://instagram.com" target="_blank" className="flex items-center text-center">
-            <AiOutlineInstagram className="h-6 w-6 p-1"/>
-            Instagram
-            </a>
-          </li>
+          <SomeLink 
+          logo={<AiOutlineInstagram 
+          className="h-6 w-6 p-1" />} 
+          text="@Turunwappuradio" 
+          href="https://instagram.com/"
+          />
+          <SomeLink 
+          logo={<AiFillFacebook className="h-6 w-6 p-1" />} 
+          text="Turun wappuradio" 
+          href="https://facebook.com/"
+          />
+          <SomeLink
+          logo={<FaDiscord className="h-6 w-6 p-1" />}
+          text="Turun wappuradion discord"
+          href="https://discorg.gg/"
+          />
+          <SomeLink
+          logo={<FaTelegramPlane className='h-6 w-6 p-1' />}
+          text="t.me/turunwappuradio"
+          href="https://t.me/turunwappuradio"
+          />
         </ul>
       </div>
-      <div>
-      <div className="relative flex h-28 w-28 lg:h-32 lg:w-32 xl:h-36 xl:w-36">
+      <div className="flex-col flex-center items-center justify-center">
+      <div className="relative mx-auto h-28 w-28 lg:h-32 lg:w-32 xl:h-36 xl:w-36">
         <Image src="/leima.svg" layout="fill" priority={true} />
       </div>
-        <div className="relative p-3 text-center text-sm text-white opacity-50">Ja tähän loput</div>
+      <Link href="/tietosuojaseloste">
+        <a className="relative p-3 text-center text-sm text-white opacity-25">
+        © Turun Wappuradio ry
+        </a>
+        <a className="relative p-3 text-center text-sm text-white opacity-25">
+          Tietoturvaseloste
+        </a>
+      </Link>
+        <ul className="relative p-3 text-center text-sm text-white opacity-25">
+          <li>
+          <a className="font-thin">© Turun Wappuradio ry</a>
+          </li>
+          <li>
+          <a>Tietoturvaseloste</a>
+          </li>
+        </ul>
       </div>
       <style jsx>{`
       li:hover {
@@ -67,6 +98,14 @@ const Footer: FC<FooterProps> = ({ navigationItems }) => {
       `}</style>
     </div>
   );
+};
+
+const SomeLink: FC<{ text: string, href: string, logo: JSX.Element }> = ({ text, href, logo }) => {
+  return <li className="text-center">
+    <a href={href} target="_blank" className="flex items-center text-center hover:underline">
+      {logo}{text}
+    </a>
+  </li>;
 };
 
 export default Footer;
