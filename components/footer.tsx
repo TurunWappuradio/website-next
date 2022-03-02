346263fbe784b1a00c8d3a9dbd19c43b140d446f
kaltsone <kvjhau@utu.fi>
2022-03-02 18:36:23 +0200

    almost good this footer now

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/Link';
 import { FC } from 'react';
-import { AiOutlineInstagram, AiFillFacebook } from 'react-icons/ai';
+import {
+  AiOutlineInstagram,
+  AiFillFacebook,
+  AiOutlineMail,
+  AiFillGithub,
+} from 'react-icons/ai';
 import { FaDiscord, FaTelegramPlane } from 'react-icons/fa';
 
 import { NavigationItem } from 'contentful/client';
@@ -10,102 +15,91 @@ interface FooterProps {
   navigationItems: NavigationItem[];
 }
 
-const Footer: FC<FooterProps> = ({ navigationItems }) => {
+const Footer = ({ navigationItems }: FooterProps) => {
   return (
-    <div className="z-15 p-6 flex flex-wrap items-center justify-center bg-blue-dark">
-      <div className="p-4 px-16 text-l text-white">
-        <ul className="space-y-2 ">
+    <div className="z-15 flex max-w-full justify-center items-center bg-blue-darkest py-6">
+      <div className="text-l w-128 text-white hidden md:block">
+        <ul className="space-y-2">
           <li key="/">
             <Link href={'/'}>
-              <a>Etusivu</a>
+              <a className="hover:underline">Etusivu</a>
             </Link>
           </li>
           {navigationItems.map((navItem) => (
             <li key={navItem.slug}>
               <Link href={'/' + navItem.slug}>
-                <a>{navItem.name}</a>
+                <a className="hover:underline">{navItem.name}</a>
               </Link>
             </li>
           ))}
-          {/* Other buttons, placeholders */}
-          <li>
-            <Link href={'/lisatietoja'}>
-              <a>Lisätietoja</a>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/yhteistyo'}>
-              <a>Yhteistyö</a>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/musiikki'}>
-              <a>Musiikki</a>
-            </Link>
-          </li>
         </ul>
       </div>
-      <div className="p-4 px-16 text-sm text-white">
-        <ul>
-          <SomeLink 
-          logo={<AiOutlineInstagram 
-          className="h-6 w-6 p-1" />} 
-          text="@Turunwappuradio" 
-          href="https://instagram.com/"
-          />
-          <SomeLink 
-          logo={<AiFillFacebook className="h-6 w-6 p-1" />} 
-          text="Turun wappuradio" 
-          href="https://facebook.com/"
+      <div className="flex w-128 justify-end text-sm text-white m-10">
+        <ul className="space-y-2">
+          <SomeLink
+            LogoComponent={AiOutlineInstagram}
+            href="https://instagram.com/turunwappuradio"
           />
           <SomeLink
-          logo={<FaDiscord className="h-6 w-6 p-1" />}
-          text="Turun wappuradion discord"
-          href="https://discorg.gg/"
+            LogoComponent={
+              AiFillFacebook
+            }
+            href="https://facebook.com/turunwappuradio/"
           />
           <SomeLink
-          logo={<FaTelegramPlane className='h-6 w-6 p-1' />}
-          text="t.me/turunwappuradio"
-          href="https://t.me/turunwappuradio"
+            LogoComponent={
+              FaDiscord
+            }
+            href="https://discord.gg/CpnVGckP9Z"
+          />
+          <SomeLink
+            LogoComponent={
+              FaTelegramPlane
+            }
+            href="https://t.me/turunwappuradio"
+          />
+          <SomeLink
+            LogoComponent={
+              AiOutlineMail
+            }
+            href="mailto:toimitus@turunwappuradio.com"
+          />
+          <SomeLink
+            LogoComponent={
+              AiFillGithub
+            }
+            href="https://github.com/TurunWappuradio"
           />
         </ul>
+      <div className="mx-6 flex max-w-full flex-col items-center">
+        <div className="relative mx-auto h-28 w-28 lg:h-32 lg:w-32 xl:h-36 xl:w-36">
+          <Image src="/leima.svg" layout="fill" priority={true} />
+        </div>
+        <Link href="/tietosuojaseloste">
+          <a className="text-sm text-white opacity-25">
+            © Turun Wappuradio ry
+            <br />
+            Tietosuojaseloste
+          </a>
+        </Link>
       </div>
-      <div className="flex-col flex-center items-center justify-center">
-      <div className="relative mx-auto h-28 w-28 lg:h-32 lg:w-32 xl:h-36 xl:w-36">
-        <Image src="/leima.svg" layout="fill" priority={true} />
       </div>
-      <Link href="/tietosuojaseloste">
-        <a className="relative p-3 text-center text-sm text-white opacity-25">
-        © Turun Wappuradio ry
-        </a>
-        <a className="relative p-3 text-center text-sm text-white opacity-25">
-          Tietoturvaseloste
-        </a>
-      </Link>
-        <ul className="relative p-3 text-center text-sm text-white opacity-25">
-          <li>
-          <a className="font-thin">© Turun Wappuradio ry</a>
-          </li>
-          <li>
-          <a>Tietoturvaseloste</a>
-          </li>
-        </ul>
-      </div>
-      <style jsx>{`
-      li:hover {
-        text-decoration:underline
-      }
-      `}</style>
     </div>
   );
 };
 
-const SomeLink: FC<{ text: string, href: string, logo: JSX.Element }> = ({ text, href, logo }) => {
-  return <li className="text-center">
-    <a href={href} target="_blank" className="flex items-center text-center hover:underline">
-      {logo}{text}
-    </a>
-  </li>;
+const SomeLink: FC<{ href: string; LogoComponent: any }> = ({ href, LogoComponent }) => {
+  return (
+    <li className="text-center">
+      <a
+        href={href}
+        target="_blank"
+        className="flex items-center transition hover:scale-150"
+      >
+        <LogoComponent className="h-8 w-8 p-1 transition hover:fill-coral" />
+      </a>
+    </li>
+  );
 };
 
 export default Footer;
