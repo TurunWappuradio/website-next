a319bea4fa4c689447a536c82b3b8a99ee795109
Werneri Aarnio <werneri.aarnio@gmail.com>
2022-03-16 09:43:44 +0200

    Add showlist querys

--- pages/arkisto/[showlistId].tsx
+++ pages/arkisto/[showlistId].tsx
@@ -1,60 +1,83 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import { ShowlistPathsDocument } from 'contentful/graphql/showlistPaths.graphql';
+import { fetchContent, fetchNavigationItems } from 'contentful/client';
+import { ShowlistPathsQuery } from 'contentful/graphql/showlistPaths.graphql';
 import {
-  ContentPagePathsDocument,
-  ContentPagePathsQuery,
-} from 'contentful/contentPagePaths.graphql';
-import {
-  fetchContent,
-  fetchNavigationItems,
-  NavigationItem,
-} from 'contentful/client';
+  ShowlistPageDocument,
+  ShowlistPageQuery,
+} from 'contentful/graphql/showlistPage.graphql';
+
+export enum Color {
+  Night = 'night',
+  Promote = 'promote',
+}
+interface ShowsCollectionItem {
+  name?: string;
+  start?: string;
+  end?: string;
+  description?: null | string;
+  picture?: Picture | null;
+  hosts?: null | string;
+  producer?: null | string;
+  color?: Color | null;
+}
+interface Picture {
+  title?: string;
+  description?: null;
+  contentType?: string;
+  fileName?: string;
+  size?: number;
+  url?: string;
+  width?: number;
+  height?: number;
+}
+interface ShowListPageProps {
+  name: string;
+  id: string;
+  shows: ShowsCollectionItem[];
+}
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const pathsResult = await fetchContent<ContentPagePathsQuery>(
-    ContentPagePathsDocument
+  const pathsResult = await fetchContent<ShowlistPathsQuery>(
+    ShowlistPathsDocument
   );
-  const paths = pathsResult.contentPageCollection.items.map((item) => ({
-    params: { slug: item.slug.split('/') },
+  const paths = pathsResult.programmeCollection.items.map((item) => ({
+    params: {
+      showlistId: item.id,
+    },
   }));
-
   return {
     paths,
     fallback: false,
   };
 };
 
-export const getStaticProps: GetStaticProps<ContentPageProps> = async (
-  context
-) => {
-  const { slug } = context.params;
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { showlistId } = context.params;
 
-  const slugJoined = typeof slug === 'string' ? slug : slug.join('/');
-
-  const data = await fetchContent<ContentPageQuery>(ContentPageDocument, {
-    slug: slugJoined,
+  const data = await fetchContent<ShowlistPageQuery>(ShowlistPageDocument, {
+    showlistId,
   });
-  const {
-    name,
-    description,
-    heroImage,
-    heroSubtext,
-    heroButtonLink,
-    heroButtonText,
-    content,
-  } = data.contentPageCollection.items[0];
+
+  const { name, id, showsCollection } = data.programmeCollection.items[0];
+  const shows = showsCollection.items.map((item) => ({
+    name: item.name,
+    start: item.start,
+    end: item.end,
+    description: item.description,
+    picture: item.picture,
+    hosts: item.hosts,
+    producer: item.producer,
+    color: item.color,
+  }));
 
   const navigationItems = await fetchNavigationItems();
 
   return {
     props: {
       name,
-      description,
-      heroImage,
-      heroSubtext,
-      heroButtonLink,
-      heroButtonText,
-      navigationItems,
-      content,
+      id,
+      shows,
     },
   };
 };
