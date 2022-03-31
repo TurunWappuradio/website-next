90a102617c9ae37bf5a6672c77067b6db95c6811
Werneri Aarnio <werneri.aarnio@gmail.com>
2022-03-31 15:44:35 +0300

    Add showlist page base

---
+++ pages/arkisto/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export const Arkisto = () => {
+  return (
+    <div className="min-h-screen w-full">
+      <Head>
+        <title>Turun Wappuradio</title>
+        <meta name="description" content="Wappuradioo tält puolt jokkee" />
+      </Head>
+    </div>
+  );
+};
+
+export default Arkisto;
