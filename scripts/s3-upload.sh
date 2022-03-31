d9d5dac284329fe8f44a333664f0078cd5c0c15d
Konsta Purtsi <konsta@identio.fi>
2022-03-31 23:16:52 +0300

    Make deploy script use aws cli

---
+++ scripts/s3-upload.sh
@@ -0,0 +1,11 @@
+# This script uploads the contents of ./out to S3.
+# All html-files are stripped of their extensions for cleaner URLs.
+
+cd ./out
+
+aws s3 cp . s3://$S3_BUCKET/ --recursive --exclude "*.html" --acl public-read
+aws s3 cp index.html s3://$S3_BUCKET/ --acl public-read
+
+for file in $(find . -name '*.html' | sed 's|^\./||'); do
+    aws s3 cp ${file%} s3://$S3_BUCKET/${file%.*} --content-type 'text/html' --acl public-read
+done
