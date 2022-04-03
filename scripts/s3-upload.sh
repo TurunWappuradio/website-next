7065e2b2baa58be41e3e24aad67fd4db86e2d05c
Konsta Purtsi <konsta@identio.fi>
2022-04-03 16:26:11 +0300

    Adds cache control header to non-html files.

--- scripts/s3-upload.sh
+++ scripts/s3-upload.sh
@@ -3,7 +3,7 @@
 
 cd ./out
 
-aws s3 cp . s3://$S3_BUCKET/ --recursive --exclude "*.html" --acl public-read
+aws s3 cp . s3://$S3_BUCKET/ --recursive --exclude "*.html" --acl public-read --cache-control max-age=604800
 aws s3 cp index.html s3://$S3_BUCKET/ --acl public-read
 
 for file in $(find . -name '*.html' | sed 's|^\./||'); do
