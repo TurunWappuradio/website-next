3bbecd12eeb4246e925eb51674c18b6bb73991c3
Konsta Purtsi <konsta@identio.fi>
2022-04-18 16:55:41 +0300

    Refactor showcard to be a bit more readable.

--- components/showcard.tsx
+++ components/showcard.tsx
@@ -30,10 +30,7 @@ export const ShowCard = ({ show, index, className, forceOpen }: ShowCard) => {
           : 'h-40'
       }`}
     >
-      <p className="mt-0 mt-1 h-[25px] w-[25px] shrink-0 rotate-90 text-left font-bold text-white shadow lg:flex">
-        {format(new Date(show.start), 'p', { locale: fi })}&nbsp;-&nbsp;
-        {format(new Date(show.end), 'p', { locale: fi })}
-      </p>
+      <Time start={show.start} end={show.end} />
       <button
         className={`group relative flex h-full w-full flex-col flex-col-reverse overflow-hidden rounded transition-all md:flex-row  ${
           isExpanded
@@ -42,45 +39,8 @@ export const ShowCard = ({ show, index, className, forceOpen }: ShowCard) => {
         }`}
         onClick={() => setIsExpanded((isExpanded) => !isExpanded)}
       >
-        <div
-          className={`absolute bottom-1 left-2 z-20 mb-2 w-full translate-y-[4.3rem] flex-col text-left text-white transition group-hover:bottom-2 group-hover:translate-y-0
-           ${isExpanded ? 'hidden' : 'block'}`}
-        >
-          <p
-            className={`w-fit rounded-sm px-2 text-base font-bold md:text-lg ${
-              index % 2 === 0 ? 'bg-coral' : 'bg-teal'
-            }`}
-          >
-            {show.name}
-          </p>
-          <p className="mt-2 px-2 text-sm">
-            Juontaa: {show.hosts ?? 'Haamujuontaja'}
-          </p>
-          <p className="mt-1 px-2 text-sm">
-            Tuottaa: {show.producer ?? 'Toimitus'}
-          </p>
-          <p className="mt-4 h-6 w-80 truncate px-2 text-sm">
-            {show.description}
-          </p>
-        </div>
-        <div
-          className={`text-box h-100% z-10 mt-auto flex-col overflow-y-auto rounded bg-blue-dark p-4 text-left transition ease-in-out md:ml-auto md:mt-0 md:w-[24rem] ${
-            isExpanded ? 'opacity-100' : 'opacity-0'
-          }`}
-        >
-          <h2 className="text-base font-bold text-teal sm:text-lg">
-            {show.name}
-          </h2>
-          <h3 className="mt-2 text-sm font-bold text-white sm:text-base">
-            Juontaa: {show.hosts ?? 'Haamujuontaja'}
-          </h3>
-          <h3 className="mt-1 text-sm font-bold text-white sm:text-base">
-            Tuottaa: {show.producer ?? 'Toimitus'}
-          </h3>
-          <p className="mt-2 text-sm text-white md:text-base">
-            {show.description}
-          </p>
-        </div>
+        <TitleInfo show={show} isExpanded={isExpanded} index={index} />
+        <Descriptions show={show} isExpanded={isExpanded} />
         <div
           ref={ref}
           className={`aspect-[3/2] w-full ${
@@ -103,14 +63,76 @@ export const ShowCard = ({ show, index, className, forceOpen }: ShowCard) => {
           />
         </div>
       </button>
-      <style jsx>{`
-        @media (min-width: 768px) {
-          .text-box {
-            max-height: ${ref?.current?.getBoundingClientRect()?.height ||
-            300}px;
-          }
-        }
-      `}</style>
+    </div>
+  );
+};
+
+interface TimeProps {
+  start: string;
+  end: string;
+}
+
+const Time = ({ start, end }: TimeProps) => {
+  const startTime = format(new Date(start), 'p', { locale: fi });
+  const endTime = format(new Date(end), 'p', { locale: fi });
+
+  return (
+    <p className="mt-1 h-[25px] w-[25px] rotate-90 font-bold text-white">
+      {startTime}&nbsp;-&nbsp;{endTime}
+    </p>
+  );
+};
+
+interface TitleInfoProps {
+  show: Show;
+  isExpanded: boolean;
+  index: number;
+}
+
+const TitleInfo = ({ show, isExpanded, index }: TitleInfoProps) => {
+  return (
+    <div
+      className={`absolute bottom-1 left-2 z-20 mb-2 w-full translate-y-[4.3rem] flex-col text-left text-white transition group-hover:bottom-2 group-hover:translate-y-0
+           ${isExpanded ? 'hidden' : 'block'}`}
+    >
+      <p
+        className={`w-fit rounded-sm px-2 text-base font-bold md:text-lg ${
+          index % 2 === 0 ? 'bg-coral' : 'bg-teal'
+        }`}
+      >
+        {show.name}
+      </p>
+      <p className="mt-2 px-2 text-sm">
+        Juontaa: {show.hosts ?? 'Haamujuontaja'}
+      </p>
+      <p className="mt-1 px-2 text-sm">
+        Tuottaa: {show.producer ?? 'Toimitus'}
+      </p>
+      <p className="mt-4 h-6 w-80 truncate px-2 text-sm">{show.description}</p>
+    </div>
+  );
+};
+
+interface DescriptionsProps {
+  show: Show;
+  isExpanded: boolean;
+}
+
+const Descriptions = ({ show, isExpanded }: DescriptionsProps) => {
+  return (
+    <div
+      className={`z-10 mt-auto flex-col overflow-y-auto rounded bg-blue-dark p-4 text-left transition ease-in-out md:ml-auto md:mt-0 md:w-[24rem] ${
+        isExpanded ? 'opacity-100' : 'opacity-0'
+      }`}
+    >
+      <h2 className="text-base font-bold text-teal sm:text-lg">{show.name}</h2>
+      <h3 className="mt-2 text-sm font-bold text-white sm:text-base">
+        Juontaa: {show.hosts ?? 'Haamujuontaja'}
+      </h3>
+      <h3 className="mt-1 text-sm font-bold text-white sm:text-base">
+        Tuottaa: {show.producer ?? 'Toimitus'}
+      </h3>
+      <p className="mt-2 text-sm text-white md:text-base">{show.description}</p>
     </div>
   );
 };
