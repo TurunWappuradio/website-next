d2332bbd4017314fb811acdbd24bedc1b7bcb022
Konsta Purtsi <konsta@identio.fi>
2022-04-19 16:29:50 +0300

    Refactor controls to their own component.

--- components/player.tsx
+++ components/player.tsx
@@ -9,40 +9,53 @@ interface PlayerProps {
 }
 
 const Player = ({ playing, onPlayPause, muted, onMute }: PlayerProps) => {
+  return (
+    <div className="bg-blue-darkest p-8">
+      <div className="mx-auto flex max-w-4xl">
+        <Controls
+          playing={playing}
+          onPlayPause={onPlayPause}
+          muted={muted}
+          onMute={onMute}
+        />
+      </div>
+    </div>
+  );
+};
+
+const Controls = ({ playing, onPlayPause, muted, onMute }: PlayerProps) => {
   const MuteIcon = muted ? BsVolumeMute : BsVolumeUp;
 
   const { song, artist } = useMetadata();
 
   return (
-    <div className="bg-blue-darkest p-8">
-      <div className="mx-auto flex max-w-4xl">
-        <button
-          onClick={onPlayPause}
-          className={`flex h-28 w-28 items-center justify-center rounded-full ${
-            playing ? 'bg-coral' : 'bg-teal'
-          }`}
-        >
-          {playing ? (
-            <BsPause size="6rem" />
-          ) : (
-            <BsPlay size="6rem" className="ml-2" />
-          )}
-        </button>
-        <button
-          onClick={onMute}
-          className={`ml-4 h-20 w-20 rounded-full ${
-            muted ? 'bg-coral' : 'bg-teal'
-          }`}
-        >
-          <MuteIcon size="3rem" className="mx-auto" />
-        </button>
-        <div className="ml-4 flex flex-col">
-          <span className="text-white">Nyt soi</span>
-          <span className="text-coral">{song}</span>
-          <span className="text-coral opacity-60">{artist}</span>
-        </div>
+    <>
+      <button
+        onClick={onPlayPause}
+        className={`flex h-28 w-28 items-center justify-center rounded-full ${
+          playing ? 'bg-coral' : 'bg-teal'
+        }`}
+      >
+        {playing ? (
+          <BsPause size="6rem" />
+        ) : (
+          <BsPlay size="6rem" className="ml-2" />
+        )}
+      </button>
+      <button
+        onClick={onMute}
+        className={`ml-4 h-20 w-20 rounded-full ${
+          muted ? 'bg-coral' : 'bg-teal'
+        }`}
+      >
+        <MuteIcon size="3rem" className="mx-auto" />
+      </button>
+      <div className="ml-4 flex flex-col">
+        <span className="text-white">Nyt soi</span>
+        <span className="text-coral">{song}</span>
+        <span className="text-coral opacity-60">{artist}</span>
       </div>
-    </div>
+    </>
   );
 };
 
