cbafaf518ba5a69e73ad7c6fa984ec31d80ec457
niemisami <sami@nieminen.fi>
2023-04-05 15:54:20 +0300

    Create google helpers for fetching sheets and files
    Requires following .env variables to work
    
    GA_SPREADSHEET_SHOWLIST
    GA_API_KEY
    GA_TYPE
    GA_PROJECT_ID
    GA_PRIVATE_KEY_ID
    GA_PRIVATE_KEY
    GA_CLIENT_EMAIL
    GA_CLIENT_ID
    GA_AUTH_URI
    GA_TOKEN_URI
    GA_AUTH_PROVIDER_X509_CERT_URL
    GA_CLIENT_X509_CERT_URL
    GA_SPREADSHEET_SHOWLIST
    GA_SPREADSHEET_RANGE
    
    Currently controlled by Sami Nieminen (nieminen.samik@gmail.com)

---
+++ google/google.ts
@@ -0,0 +1,81 @@
+import * as gDrive from '@googleapis/drive';
+import * as gSheets from '@googleapis/sheets';
+import * as auth from 'google-auth-library';
+
+const credentials = {
+  'private_key': process.env.GA_PRIVATE_KEY?.replace(/\\n/gm, '\n'),
+  'client_email': process.env.GA_CLIENT_EMAIL
+};
+
+export type GoogleConfigSheets = {
+  apiKey: string,
+  spreadsheetId: string,
+  range: string,
+}
+
+export type GoogleConfigFiles = {
+  apiKey: string,
+  fileId: string,
+  fields?: string[]
+}
+
+const getJwt = (googleService: 'spreadsheets'|'drive') => {
+  return new auth.JWT(
+    credentials.client_email, 
+    undefined, 
+    credentials.private_key,
+    [`https://www.googleapis.com/auth/${googleService}`]
+  );
+};
+
+export const getSheet = async (config: GoogleConfigSheets):Promise<gSheets.sheets_v4.Schema$ValueRange|null> => {
+  if(!config.apiKey) {
+    return null;
+  }
+  const jwt = getJwt('spreadsheets');
+  return getSheetRequest(jwt, config.apiKey, config.spreadsheetId, config.range);
+};
+
+const getSheetRequest = async (jwt: auth.JWT, apiKey: string, spreadsheetId: string, range: string) => {
+  const sheets = gSheets.sheets({ version: 'v4' });
+  try {
+    const result = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+      auth: jwt,
+      key: apiKey
+    }); 
+    return result.data;
+  } catch (error) {
+    console.dir(error, { depth: null });
+    throw error;
+  }
+};
+
+export const getFile = (config: GoogleConfigFiles) => {
+  const jwt = getJwt('drive');
+  return getFileRequest(jwt, config.apiKey, config.fileId, config.fields);
+};
+
+export const getFileRequest = async (jwt: auth.JWT, apiKey: string, fileId: string, fields?: string[]) => {
+  const drive = gDrive.drive({
+    version: 'v3',
+    auth: jwt,
+    key: apiKey
+  });
+  try {
+    // List of file fields https://developers.google.com/drive/api/v3/reference/files
+    const fieldString = fields?.join(',') || null;
+    const result = await drive.files.get({ 
+      fileId,
+      fields: fieldString
+    });
+    if(result.status !== 200) {
+      throw new Error(`Failed to load file ${fileId}`);
+    }
+    return result;
+  } catch (error) {
+    console.dir(error, { depth: null });
+    throw error;
+  }
+};
