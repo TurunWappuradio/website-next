9f6992a404217a2bd46679443b37c579151fe051
vurpo <vurpo@vurpo.fi>
2023-04-28 12:32:05 +0300

    Fix telegram badge layout (#55)

--- components/ShoutBox/nameformatter.tsx
+++ components/ShoutBox/nameformatter.tsx
@@ -16,7 +16,8 @@ const NameFormatter = ({ name, timestamp, telegram }: NameFormatterProps) => {
       className={`flex items-center pr-2 text-sm font-bold ${findStyle(name)}`}
     >
       {name}
-      {telegram ? <a target="_blank" rel="noreferrer" href="https://t.me/turunwappuradio"><Image alt="Telegram" className="ml-2 w-3.5 h-3.5" src="/telegram.svg"></Image></a> : null}
+      {telegram ? <div className="relative ml-2 w-3.5 h-3.5"><a target="_blank" rel="noreferrer" href="https://t.me/turunwappuradio"><Image layout="fill"
+        unoptimized={true} alt="Telegram" src="/telegram.svg"></Image></a></div> : null}
       {
         time ? (
           <span className="pl-2 text-xs font-normal opacity-70" > {time}</span>
