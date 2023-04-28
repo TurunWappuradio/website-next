db9fd292b1046febe64f3f4ff4e11d0fbdd18253
vurpo <vurpo@vurpo.fi>
2023-04-28 12:00:32 +0300

    Add telegram badge for shoutbox messages from tg (#54)
    * Add telegram badge for shoutbox messages from tg
    
    * Fix use of img to Image

--- components/ShoutBox/nameformatter.tsx
+++ components/ShoutBox/nameformatter.tsx
@@ -1,12 +1,14 @@
 import { format } from 'date-fns';
 import React from 'react';
+import Image from 'next/image';
 
 interface NameFormatterProps {
   name: string;
   timestamp?: number;
+  telegram?: boolean;
 }
 
-const NameFormatter = ({ name, timestamp }: NameFormatterProps) => {
+const NameFormatter = ({ name, timestamp, telegram }: NameFormatterProps) => {
   const time = timestamp ? format(new Date(timestamp), 'HH:mm') : null;
 
   return (
@@ -14,10 +16,13 @@ const NameFormatter = ({ name, timestamp }: NameFormatterProps) => {
       className={`flex items-center pr-2 text-sm font-bold ${findStyle(name)}`}
     >
       {name}
-      {time ? (
-        <span className="pl-2 text-xs font-normal opacity-70">{time}</span>
-      ) : null}
-    </div>
+      {telegram ? <a target="_blank" rel="noreferrer" href="https://t.me/turunwappuradio"><Image alt="Telegram" className="ml-2 w-3.5 h-3.5" src="/telegram.svg"></Image></a> : null}
+      {
+        time ? (
+          <span className="pl-2 text-xs font-normal opacity-70" > {time}</span>
+        ) : null
+      }
+    </div >
   );
 };
 
