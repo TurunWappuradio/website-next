aa1e4af9d6c0e32395509dac243534c8600f3f3c
niemisami <sami@nieminen.fi>
2024-01-20 09:21:17 +0200

    Disable /api/archiver route with env

--- pages/api/archiver.ts
+++ pages/api/archiver.ts
@@ -5,10 +5,20 @@ type ResponseData = {
   message: string;
 };
 
+const isRouteEnabled = !!process.env.ENABLE_ARCHIVER_API;
+
+/**
+ * For local use only! Set `ENABLE_ARCHIVER_API` env to enable.
+ * Add data to `archiveOldShowlists` function and start archiving by calling `/api/archiver` route
+ */
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
+  if (!isRouteEnabled) {
+    return res.redirect('/404');
+  }
+
   try {
     await archiveOldShowlists();
     res.status(200).json({ message: 'OK' });
