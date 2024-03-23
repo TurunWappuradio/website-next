f1151cba00338fce24141c8c5b1d0548fbf1046b
ebetoi <toivonen.ebe@gmail.com>
2024-03-23 16:28:07 +0200

    Get changes from #74 to feat/esittelyt

--- pages/esittelyt.tsx
+++ pages/esittelyt.tsx
@@ -6,17 +6,17 @@ import Image from 'next/image';
 import {
   EsittelytDocument,
   EsittelytQuery,
-} from 'contentful/graphql/esittelytPage.graphql';
+} from '@/contentful/graphql/esittelytPage.graphql';
 import {
   fetchContent,
   fetchNavigationItems,
   NavigationItem,
-} from 'contentful/client';
+} from '@/contentful/client';
 
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-import Footer from 'components/footer';
-import Hero from 'components/hero';
+import Footer from '@/components/footer';
+import Hero from '@/components/hero';
 
 interface EsittelytProps {
   navigationItems?: NavigationItem[] | null;
