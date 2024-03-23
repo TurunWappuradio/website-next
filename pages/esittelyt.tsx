f885af7d49936cc084f00025f593a0cd128b3b50
ebetoi <toivonen.ebe@gmail.com>
2024-03-23 13:51:43 +0200

    Esittelyt fixes

--- pages/esittelyt.tsx
+++ pages/esittelyt.tsx
@@ -1,48 +1,48 @@
-import { EsittelytDocument, EsittelytQuery } from 'contentful/graphql/esittelytPage.graphql';
-import { fetchContent, fetchNavigationItems, NavigationItem } from 'contentful/client';
-import { contentfulImageLoader } from 'contentful/contentfulImageLoader';
-//import { props } from 'ramda';
-
+import { useEffect, useState } from 'react';
 import { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
+
+import {
+  EsittelytDocument,
+  EsittelytQuery,
+} from 'contentful/graphql/esittelytPage.graphql';
+import {
+  fetchContent,
+  fetchNavigationItems,
+  NavigationItem,
+} from 'contentful/client';
 
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { Document } from '@contentful/rich-text-types';
-
 
 import Footer from 'components/footer';
 import Hero from 'components/hero';
 
-import { useEffect, useState } from 'react';
-
 interface EsittelytProps {
   navigationItems?: NavigationItem[] | null;
-  esittelyt:Esittely[];
+  esittelyt: Esittely[];
 }
 
 interface Esittely {
-  name?:string | null
-  description?: any
-  index: number
-  picture?:{
-    url?:string | null
-  }
-  forceOpen?: boolean
-  className?: string
+  name?: string | null;
+  description?: any;
+  picture?: {
+    url?: string | null;
+  };
+  forceOpen?: boolean;
+  className?: string;
 }
 
 const ExhibitionListCard: React.FC<{ esittely: Esittely }> = ({ esittely }) => {
-  const { name, description, index, picture, forceOpen, className} = esittely;
-  const [isExpanded, setIsExpanded] = useState<boolean>(forceOpen ?? false);
+  const { name, description, picture, className } = esittely;
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
   const handleClick = () => {
     setIsExpanded((prev) => !prev);
   };
 
   return (
     <div
-      className={`flex w-full rounded my-4 ${className} ${
+      className={`my-4 flex w-full rounded ${className} ${
         isExpanded
           ? 'h-auto rounded-l-none rounded-t lg:rounded-l lg:rounded-t-none'
           : 'h-40'
@@ -56,104 +56,77 @@ const ExhibitionListCard: React.FC<{ esittely: Esittely }> = ({ esittely }) => {
         }`}
         onClick={handleClick}
       >
-        <TitleInfo esittely={esittely} isExpanded={isExpanded} index={index} />
+        <TitleInfo esittely={esittely} isExpanded={isExpanded} />
         <Descriptions esittely={esittely} isExpanded={isExpanded} />
-        <ShowImage esittely={esittely} isExpanded={isExpanded} />
+        <PresImage esittely={esittely} isExpanded={isExpanded} />
       </button>
     </div>
   );
 };
 
-
-const EsittelytPage: NextPage<EsittelytProps> = ({ navigationItems, esittelyt }) => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchData() {
-      const data = await fetchContent<EsittelytQuery>(EsittelytDocument);
-      setLoading(false);
-    }
-    
-    fetchData();
-  }, []);
+const EsittelytPage: NextPage<EsittelytProps> = ({
+  navigationItems,
+  esittelyt,
+}) => {
+  const data = fetchContent<EsittelytQuery>(EsittelytDocument);
 
   return (
-  <div className="min-h-screen w-full">
-    <Head>
-      <title>Turun Wappuradion Hallitus ja Tiimivastaavat</title>
-      <meta
-        name="description"
-        content="Turun Wappuradion Hallitus ja Tiimivastaavat"
+    <div className="min-h-screen w-full">
+      <Head>
+        <title>Turun Wappuradion Hallitus ja Tiimivastaavat</title>
+        <meta
+          name="description"
+          content="Turun Wappuradion Hallitus ja Tiimivastaavat"
+        />
+      </Head>
+      <Hero
+        title="Esittelyt"
+        subtext="Turun Wappuradion Hallitus ja Tiimivastaavat"
+        navigationItems={navigationItems}
+        isCompact={true}
       />
-    </Head>
-    <Hero
-      title="Esittelyt"
-      subtext="Turun Wappuradion Hallitus ja Tiimivastaavat"
-      navigationItems={navigationItems}
-      isCompact={true}
-    />
-    <article className="mx-auto max-w-4xl px-4 pb-20 pt-12 text-white">
-      <h1 className="my-4 text-3xl font-bold text-coral">Ketä me ollaan?</h1>
-      {loading ? (
-          <p>Lataa...</p>
-        ) : (
+      <article className="mx-auto max-w-4xl px-4 pb-20 pt-12 text-white">
+        <h1 className="my-4 text-3xl font-bold text-coral">Ketä me ollaan?</h1>
+        {
           <div>
-            {esittelyt.map((esittely, index) => (
-              <ExhibitionListCard key={index} esittely={esittely} />
+            {esittelyt.map((esittely) => (
+              <ExhibitionListCard key={esittely.name} esittely={esittely} />
             ))}
           </div>
-        )}
-    </article>
-    <Footer navigationItems={navigationItems} />
-  </div>
+        }
+      </article>
+      <Footer navigationItems={navigationItems} />
+    </div>
   );
 };
 
-export const getStaticProps:GetStaticProps<EsittelytProps> = async () => {
+export const getStaticProps: GetStaticProps<EsittelytProps> = async () => {
   const data = await fetchContent<EsittelytQuery>(EsittelytDocument);
+
   const navigationItems = await fetchNavigationItems();
 
-  const items = data?.hallitusJaTiimiesittelyt2024Collection?.items;
-    const esittelyt: Esittely[] = items
-      ? items.map((item: any) => ({
-          name: item.name || null,
-          description:{
-            json: item.description.json || null
-          },
-          picture: {
-            url: item.picture?.url || null
-          },
-          index: item.index || 0,
-          forceOpen: item.forceOpen || false,
-          className: item.className || ''
-        }))
-      : [];
+  const esittelyt: Esittely[] = data.presentationCollection?.items || [];
 
-  return  {
-    props:{
+  return {
+    props: {
+      esittelyt,
       navigationItems,
-      esittelyt
-    }
+    },
   };
 };
 
 interface TitleInfoProps {
-  esittely: Esittely
-  isExpanded: boolean
-  index: number
+  esittely: Esittely;
+  isExpanded: boolean;
 }
 
-const TitleInfo = ({ esittely, isExpanded, index }: TitleInfoProps) => {
+const TitleInfo = ({ esittely, isExpanded }: TitleInfoProps) => {
   return (
     <div
       className={`absolute bottom-1 z-20 max-w-full flex-col px-2 text-left text-white
            ${isExpanded ? 'hidden' : 'block'}`}
     >
-      <p
-        className={`w-fit rounded-sm px-2 text-base font-bold md:text-lg ${
-          index % 2 === 0 ? 'bg-coral' : 'bg-teal'
-        }`}
-      >
+      <p className={`w-fit rounded-sm px-2 text-base font-bold md:text-lg`}>
         {esittely.name}
       </p>
     </div>
@@ -161,34 +134,35 @@ const TitleInfo = ({ esittely, isExpanded, index }: TitleInfoProps) => {
 };
 
 interface DescriptionsProps {
-  esittely: Esittely
-  isExpanded: boolean
+  esittely: Esittely;
+  isExpanded: boolean;
 }
 
 const Descriptions = ({ esittely, isExpanded }: DescriptionsProps) => {
-  
   return (
     <div
       className={`z-10 mt-auto flex flex-col overflow-y-auto rounded bg-blue-dark p-4 text-left transition ease-in-out md:ml-auto md:mt-0 md:h-[20rem] xl:h-[25rem] ${
         isExpanded ? 'opacity-100' : 'opacity-0'
       }`}
     >
-      <h2 className="text-base font-bold text-teal sm:text-lg">{esittely.name}</h2>
+      <h2 className="text-base font-bold text-teal sm:text-lg">
+        {esittely.name}
+      </h2>
       <div className="mt-2 text-sm text-white md:text-base">
-        {esittely.description && esittely.description.json ? (
-            documentToReactComponents(esittely.description.json)
-          ) : null}
+        {esittely.description && esittely.description.json
+          ? documentToReactComponents(esittely.description.json)
+          : null}
       </div>
     </div>
   );
 };
 
-interface ShowImageProps {
+interface PresImageProps {
   esittely: Esittely;
   isExpanded: boolean;
 }
 
-const ShowImage = ({ esittely, isExpanded }: ShowImageProps) => {
+const PresImage = ({ esittely, isExpanded }: PresImageProps) => {
   const { picture, name } = esittely;
   const url = picture.url;
 
